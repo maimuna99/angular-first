21f5ae430fb3de4cc6919899cdb41983bd99096a
Your Name <you@example.com>
2023-06-09 11:17:56 +0200

    styling

--- Client/src/app/components/childrenComponents/mainstore/childrenComponents/checkout/checkout.component.ts
+++ Client/src/app/components/childrenComponents/mainstore/childrenComponents/checkout/checkout.component.ts
@@ -7,13 +7,26 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./checkout.component.css'],
 })
 export class CheckoutComponent {
-  checkoutForm = new FormGroup({
-    firstName: new FormControl('', Validators.required),
-    lastName: new FormControl('', Validators.required),
-    email: new FormControl('', Validators.required),
-    country: new FormControl('', Validators.required),
-    address: new FormControl('', Validators.required),
-    zipCode: new FormControl('', Validators.required),
-    state: new FormControl('', Validators.required),
-  });
+  checkoutForm: FormGroup<{ firstName: FormControl<string | null>; lastName: FormControl<string | null>; email: FormControl<string | null>; country: FormControl<string | null>; address: FormControl<string | null>; zipCode: FormControl<string | null>; state: FormControl<string | null>; checkbox: FormControl<string | null>; }>;
+  isStateRequired(): string | boolean {
+    throw new Error('Method not implemented.');
+  }
+  onCountryChange(arg0: any) {
+    throw new Error('Method not implemented.');
+  }
+  
+
+  constructor() {
+    this.checkoutForm = new FormGroup({
+      firstName: new FormControl('', Validators.required),
+      lastName: new FormControl('', Validators.required),
+      email: new FormControl('', Validators.required),
+      country: new FormControl('', Validators.required),
+      address: new FormControl('', Validators.required),
+      zipCode: new FormControl('', Validators.required),
+      state: new FormControl(''),
+      checkbox: new FormControl('', Validators.required),
+    });
+  }
+  
 }
